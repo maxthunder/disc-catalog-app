6b3a9ba1f68edabe1ff800564f8fc7a1c56b197c
maxthunder <maxwell.stark@gmail.com>
2020-09-06 22:30:05 -0600

    Adds menu bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import { CatalogComponent } from './catalog/catalog.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CatalogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
