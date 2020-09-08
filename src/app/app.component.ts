1f1df7666bb52c67ab9ca5bd097dcd52f33fed81
maxthunder <maxwell.stark@gmail.com>
2020-09-08 17:42:11 -0600

    Adds okta sign-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {OktaAuthService} from "@okta/okta-angular";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'disc-catalog-app';
+export class AppComponent implements OnInit{
+  isAuthenticated: boolean;
+
+  constructor(public oktaAuth: OktaAuthService, public router: Router) {
+    // Subscribe to authentication state changes
+    this.oktaAuth.$authenticationState.subscribe(
+        (isAuthenticated: boolean)  => this.isAuthenticated = isAuthenticated
+    );
+  }
+
+  async ngOnInit() {
+    // Get the authentication state for immediate use
+    this.isAuthenticated = await this.oktaAuth.isAuthenticated();
+  }
+
+  login() {// might be able to remove this method, I believe it is unused
+    // this.oktaAuth.loginRedirect('/profile');
+  }
+
+  async logout() {
+    // Terminates the session with Okta and removes current tokens.
+    await this.oktaAuth.logout();
+    this.router.navigateByUrl('/');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,62 @@
-import 'hammerjs';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import {
+  OKTA_CONFIG,
+  OktaAuthModule,
+  OktaCallbackComponent,
+  OktaAuthGuard
+} from '@okta/okta-angular';
+
 import { AppComponent } from './app.component';
-import { FormsModule } from "@angular/forms";
-import { CatalogComponent } from './catalog/catalog.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CommonModule } from "@angular/common";
-import { HttpClientModule } from "@angular/common/http";
+import { LoginComponent } from "./login/login.component";
+import {ProtectedComponent} from "./protected/protected.component";
 
+const config = {
+  issuer: 'https://dev-705927.okta.com/oauth2/default',
+  redirectUri: 'http://localhost:4200/implicit/callback',
+  clientId: '0oawaajgtyCxmhZ7R4x6',
+  pkce: true
+}
+
+export function onAuthRequired({ oktaAuth, router }) {
+  // Redirect the user to your custom login page
+  router.navigate(['/login']);
+}
+
+const appRoutes: Routes = [
+  {
+    path: 'implicit/callback',
+    component: OktaCallbackComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'protected',
+    component: ProtectedComponent,
+    canActivate: [ OktaAuthGuard ],
+    data: {
+      onAuthRequired
+    }
+  }
+]
 @NgModule({
   declarations: [
     AppComponent,
-    CatalogComponent
+    LoginComponent,
+    ProtectedComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
+    RouterModule.forRoot(appRoutes),
+    OktaAuthModule
   ],
-  providers: [],
+  providers: [
+    { provide: OKTA_CONFIG, useValue: config },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
